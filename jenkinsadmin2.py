d93d5f366eed68dd9909a522f4e5ddeffecbc99e
svukelic <sanja.vukelic@avisto-eastern.com>
2022-12-14 16:58:51 +0100

    api

--- jenkinsadmin2.py
+++ jenkinsadmin2.py
@@ -1,69 +1,81 @@
-import docker
-import jenkins
+from api4jenkins import Jenkins
 import sys
-import requests
+import requests 
+import docker 
 from docker.api import container
 from configparser import ConfigParser
 import argparse
 
 parser = argparse.ArgumentParser(description = 'Jenkins server')
 parser.add_argument('-s','--action_server', metavar='', help='stop,start,backup -Jenkins server.')
-parser.add_argument('-c','--containerID', metavar='',required=True,  help='ID of Jenkins server container.')
 parser.add_argument('-j','--action_job', metavar='', help='build or stop -job.')
-parser.add_argument('-n','--job_name', metavar='', help='Name of job.')
+parser.add_argument('-n','--job_name', metavar='', help='Name of job to build/stop.')
 parser.add_argument('-b','--backup_name', metavar='', help='Backup name.')
+parser.add_argument('-c','--container', metavar='', help='container ID.')
 args = parser.parse_args()
 
 
-#Docker
+
+
+config = ConfigParser()
+config.read("parametersjenkins.ini")
+
+config_data = config["JENKINS"]
+hostJenkins = config_data['hostjenkins']
+usernameJenkins = config_data['usernamejenkins']
+passwordJenkins = config_data['passwordjenkins']
+url_projects = config_data['url_projects']
+
+server = Jenkins(hostJenkins, auth=(usernameJenkins,passwordJenkins))
+
+
 client = docker.from_env()
-container = client.containers.get(args.containerID)
 
-
-#main
+#server
 action_server = args.action_server
 if action_server == "stop":
-    container.stop()
+    server.system.quiet_down()
     print("Jenkins instance was stopped.")
 elif action_server == "start":
-    container.start()
+    server.system.cancel_quiet_down()
     print("Jenkins instance was started.")
 
 elif action_server == "backup":
-    nameB = args.backup_name
-    container.commit(nameB)
-    print("Jenkins instance backup was made.")
-
-
-
-action_job = args.action_job
-if container.status == "running":
-    config = ConfigParser()
-    config.read("parametersjenkins.ini")
-
-    config_data = config["JENKINS"]
-    hostJenkins = config_data['hostjenkins']
-    usernameJenkins = config_data['usernamejenkins']
-    passwordJenkins = config_data['passwordjenkins']
-    url_projects = config_data['url_projects']
-
-    server = jenkins.Jenkins(hostJenkins, username=usernameJenkins, password=passwordJenkins)
-    response = requests.get(url_projects)
-    user = server.get_whoami()
-    version = server.get_version()
-    print(f'Jenkins user: {user["fullName"]} Jenkins version: {version}')
-    jobs = server.get_jobs()
-    if action_job == "build":       
-       job_name = args.job_name
-       server.build_job(job_name)
-       print("Job-build.")
-
-    elif action_job == "stop":
-       job_name = args.job_name
-       job_number = args.job_number
-       server.stop_build(job_name, job_number)
+    if args.backup_name:
+       if args.container:
+          IDcontainer = args.container
+          container = client.containers.get(IDcontainer)
+          nameB = args.backup_name
+          container.commit(nameB)
+          print("Jenkins instance backup was made.")
+       else:
+          print("Add containerID.(-c)")
     else:
-       print("Wrong action_job argument.(build, stop)")
-else:
-    print("Start Jenkins server.(arguments: -s start -c containerID)")
+       print("Add backup_name.(-n)")
+
+
+
+#job
+job_name = args.job_name
+action_job = args.action_job
+
+if action_job == "build":
+    if args.job_name:
+       job = server.get_job(job_name)       
+       job.build()
+       print(server.build_job(job_name))
+       print("Job-build.")
+    else:
+       print("Add job_name.(-n)")
+
+elif action_job == "stop":
+    if args.job_name:
+       job = server.get_job(job_name)
+       build = job.get_last_build()
+       print (build)
+       build.stop()
+    else:
+       print("Add job_name.(-n)")
+
+
 
